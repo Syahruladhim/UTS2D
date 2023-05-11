d47b8a6f43dfe4b025da20deb76ade6038ab2c93
Syahruladhim <117799089+Syahruladhim@users.noreply.github.com>
2023-05-11 18:31:24 +0700

    Add files via upload

---
+++ header1.php
@@ -0,0 +1,18 @@
+
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo APPNAME; ?></title>
+
+    <!-- BOOTSTRAP -->
+
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./sweetalert/sweetalert.css">
+
+    <!-- JQuery and BootStrap Scripts Loading -->
+
+    <script src="./jQuery/jquery-3.5.1.js"></script>
+    <script src="./bootstrap/js/bootstrap.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./sweetalert/sweetalert.min.js"></script>
